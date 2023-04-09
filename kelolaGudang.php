4d9ac62d2e7900461608fd61378b97d19dd77441
ashrafnawaf <ashrafnawaf07@gmail.com>
2023-04-10 00:05:48 +0700

    update kelola gudang, kelola petugas

--- kelolaGudang.php
+++ kelolaGudang.php
@@ -1,59 +1,153 @@
 <?php 
-    require_once('sidebarnav.php');
+    include 'sidebarnav.php';
     include_once 'config.php';
+    ob_start();
+
+    $redirect_path = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'] ;
+    if(isset($_POST['create'])) {
+        $id_gudang = $conn->real_escape_string($_POST['id_gudang']);
+        $jumlah = $conn->real_escape_string($_POST['jumlah']);
+        $kandungan_lemak = $conn->real_escape_string($_POST['kandungan_lemak']);
+        $kandungan_protein = $conn->real_escape_string($_POST['kandungan_protein']);
+
+        $sql = "INSERT INTO gudang (id_gudang, jumlah, kandungan_lemak, kandungan_protein) VALUES ('$id_gudang', '$jumlah', '$kandungan_lemak', '$kandungan_protein')";
+        $conn->query($sql) or die(mysqli_error($conn));
+        ?>
+        <script>
+            window.location.assign("<?= $redirect_path?>")
+        </script>
+        <?php
+    }  
+    
+    if (isset($_POST['update'])) {
+        $id_gudang = $conn->real_escape_string($_POST['update']);
+        $jumlah = $conn->real_escape_string($_POST['jumlah']);
+        $kandungan_lemak = $conn->real_escape_string($_POST['kandungan_lemak']);
+        $kandungan_protein = $conn->real_escape_string($_POST['kandungan_protein']);
+
+        $sql = "UPDATE gudang SET jumlah = '$jumlah', kandungan_lemak = '$kandungan_lemak', kandungan_protein = '$kandungan_protein' WHERE id_gudang = '$id_gudang'";
+        $conn->query($sql) or die(mysqli_error($conn));
+        ?>
+        <script>
+            window.location.assign("<?= $redirect_path?>")
+        </script>
+        <?php
+    }  
+    
+    if (isset($_POST['delete'])) {
+        $id_mitra = $conn->real_escape_string($_POST['delete']);
+        $sql = "DELETE FROM gudang WHERE id_gudang = '$id_gudang'";
+        $conn->query($sql) or die(mysqli_error($conn));
+        ?>
+        <script>
+            window.location.assign("<?= $redirect_path?>")
+        </script>
+        <?php
+    }
 ?>
+
 <!-- Page wrapper  -->
         <!-- ============================================================== -->
         <div class="page-wrapper">
             <div class="container-fluid">
                 <div class="page-breadcrumb">
                     <div class="row align-items-center">
-                        <div class="align-self-center">
-                            <h3 class="page-title mb-0 p-0">Kelola Gudang</h3>
+                        <div class="align-self-center d-flex gap-3">
+                            <h3 class="page-title mb-0 p-0">Gudang Susu</h3>
+                            <?php if(!isset($_GET['add'])): ?>
+                                <a href="?add=true" class="btn btn-success">
+                                    Tambah Data Gudang Susu +
+                                </a>
+                            <?php endif?>
                         </div>
 
                     <!-- table  -->
-                    <div class="mt-4 align-items-right">
-                        <div class="text-end upgrade-btn">
-                            <a href="tambahPencatatan.php"
-                                class="btn btn-success d-none d-md-inline-block text-white">Add Data Baru <i class="fa-solid fa-plus"></i></a>
-                        </div>
-                    </div>
-                    <div class="my-4">
+                    <?php if(isset($_GET['add'])): ?>
+                        <form class="mt-2" action="" method="post">
+                            <div class="form-group">
+                                <label for="id_gudang">Id Gudang</label>
+                                <input id="id_gudang" name="id_gudang" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="jumlah">Jumlah</label>
+                                <input id="jumlah" name="jumlah" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="kandungan_lemak">Kandungan Lemak</label>
+                                <input id="kandungan_lemak" name="kandungan_lemak" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="kandungan_protein">Kandungan Protein</label>
+                                <input id="kandungan_protein" name="kandungan_protein" type="text" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-block btn-success" name="create">Tambah</button>
+                        </form>
+                    <?php endif?>
+
+                    <?php if(isset($_GET['edit'])): ?>
+                        <form class="mt-2" action="" method="post">
+                            <div class="form-group">
+                                <label for="id_gudang">Id Gudang</label>
+                                <input id="id_gudang" name="id_gudang" type="text" class="form-control" value="<?= $_GET['edit']?>" disabled>
+                            </div>
+                            <div class="form-group">
+                                <label for="jumlah">Jumlah</label>
+                                <input id="jumlah" name="jumlah" type="text" class="form-control" value="<?= $_GET['jumlah']?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="kandungan_lemak">Kandungan Lemak</label>
+                                <input id="kandungan_lemak" name="kandungan_lemak" type="text" class="form-control" value="<?= $_GET['kandungan_lemak']?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="kandungan_protein">Kandungan Protein</label>
+                                <input id="kandungan_protein" name="kandungan_protein" type="text" class="form-control" value="<?= $_GET['kandungan_protein']?>">
+                            </div>
+                            <button type="submit" class="btn btn-block btn-success" name="update" value="<?= $_GET['edit']?>">Ubah</button>
+                        </form>
+                    <?php endif?>
+
+                    <?php if(!isset($_GET['add']) && !isset($_GET['edit'])): ?>
+                        <div class="my-4">
                         <table id="example" class="table table-striped" style="width:100%">
                             <thead>
                                 <tr>
-                                    <th>id_Transaksi</th>
-                                    <th>Jumlah </th>
+                                    <th>No</th>
+                                    <th>Jumlah</th>
                                     <th>Kandungan Lemak</th>
-                                    <th>Kandungan Protein</th>
-                                    
+                                    <th>Kandungan protein</th>
+                                    <th>Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php 
-                                    $result = mysqli_query($conn, "SELECT gd.id_gudang,gd.jumlah,gd.kandungan_lemak,gd.kandungan_protein FROM gudang gd;");
-                                    $number = 1;
-                                    while($data = mysqli_fetch_array($result)) {         
-                                        echo "<tr>";
-                                        // echo "<td>".$data['id_gudang']."</td>";
-                                        echo "<td>".$number++."</td>";
-                                        echo "<td>".$data['jumlah']."</td>";
-                                        echo "<td>".$data['kandungan_lemak']."</td>";    
-                                        echo "<td>".$data['kandungan_protein']."</td>";        
-                                        echo "<td><a href='edit.php?id=$data[id_gudang]' class='btn btn-warning'><i class='fa-solid fa-pen-to-square'></i> Edit</a>
-                                        <a href='edit.php?id=$data[id_gudang]' class='btn btn-danger'><i class='fa-solid fa-xmark'></i> Delete</a></td></tr>";        
-                                    }
-                                ?>
+                                <?php $result = mysqli_query($conn, "SELECT * FROM gudang"); ?>
+                                <?php while ($data = mysqli_fetch_array($result)): ?>
+                                    <tr>
+                                        <td><?= $data['id_gudang'] ?></td>
+                                        <td><?= $data['jumlah'] ?></td>
+                                        <td><?= $data['kandungan_lemak'] ?></td>
+                                        <td><?= $data['kandungan_protein'] ?></td>
+                                        <td class="d-flex gap-3">
+                                            <a class="btn btn-outline-primary" href="?edit=<?= $data['id_gudang'] ?>&jumlah=<?= $data['jumlah']?>&kandungan_lemak=<?= $data['kandungan_lemak']?>&kandungan_protein=<?= $data['kandungan_protein']?>">Ubah</a>
+
+                                            <form action="" method="post">
+                                                <button type="submit" class="btn btn-outline-danger" name="delete" value="<?= $data['id_gudang'] ?>">Hapus</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                <?php endwhile?>
                             </tbody>
                         </table>
                     </div>
+                    <?php endif?>
 
                     </div>
                 </div>        
             </div>
+            <?php require_once('footer.php') ?>
+        </div>
+    <!-- ============================================================== -->
+<!-- End Wrapper -->
 
-         <?php require_once('sidebarnav.php') ?>   
 <script>
     $(document).ready(function () {
     $('#example').DataTable();
--- kelolaPetugas.php
+++ kelolaPetugas.php
@@ -1,57 +1,172 @@
 <?php 
     include 'sidebarnav.php';
     include_once 'config.php';
+    ob_start();
+
+    $redirect_path = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'] ;
+    if(isset($_POST['create'])) {
+        $id_petugas = $conn->real_escape_string($_POST['id_petugas']);
+        $nama = $conn->real_escape_string($_POST['nama']);
+        $no_hp = $conn->real_escape_string($_POST['no_hp']);
+        $alamat = $conn->real_escape_string($_POST['alamat']);
+        $username = $conn->real_escape_string($_POST['username']);
+        $id_role = $conn->real_escape_string($_POST['id_role']);
+
+        $sql = "INSERT INTO petugas (id_petugas, nama , no_hp, alamat, username, id_role) VALUES ('$id_petugas', '$nama', '$no_hp', '$alamat', '$username', '$id_role')";
+        $conn->query($sql) or die(mysqli_error($conn));
+        ?>
+        <script>
+            window.location.assign("<?= $redirect_path?>")
+        </script>
+        <?php
+    }  
+    
+    if (isset($_POST['update'])) {
+        $id_petugas = $conn->real_escape_string($_POST['update']);
+        $nama = $conn->real_escape_string($_POST['nama']);
+        $no_hp = $conn->real_escape_string($_POST['no_hp']);
+        $alamat = $conn->real_escape_string($_POST['alamat']);
+        $username = $conn->real_escape_string($_POST['username']);
+        $id_role = $conn->real_escape_string($_POST['id_role']);
+
+
+
+        $sql = "UPDATE petugas SET nama = '$nama', no_hp = '$no_hp', alamat = '$alamat', username = '$username', id_role = '$id_role'  WHERE id_petugas = '$id_petugas'";
+        $conn->query($sql) or die(mysqli_error($conn));
+        ?>
+        <script>
+            window.location.assign("<?= $redirect_path?>")
+        </script>
+        <?php
+    }  
+    
+    if (isset($_POST['delete'])) {
+        $id_mitra = $conn->real_escape_string($_POST['delete']);
+        $sql = "DELETE FROM petugas WHERE id_petugas = '$id_petugas'";
+        $conn->query($sql) or die(mysqli_error($conn));
+        ?>
+        <script>
+            window.location.assign("<?= $redirect_path?>")
+        </script>
+        <?php
+    }
 ?>
+
 <!-- Page wrapper  -->
         <!-- ============================================================== -->
         <div class="page-wrapper">
             <div class="container-fluid">
                 <div class="page-breadcrumb">
                     <div class="row align-items-center">
-                        <div class="align-self-center">
-                            <h3 class="page-title mb-0 p-0">Kelola Petugas</h3>
+                        <div class="align-self-center d-flex gap-3">
+                            <h3 class="page-title mb-0 p-0">Data Petugas</h3>
+                            <?php if(!isset($_GET['add'])): ?>
+                                <a href="?add=true" class="btn btn-success">
+                                    Tambah Data Petugas +
+                                </a>
+                            <?php endif?>
                         </div>
 
                     <!-- table  -->
-                    <div class="mt-4 align-items-right">
-                        <div class="text-end upgrade-btn">
-                            <a href="tambahPencatatan.php"
-                                class="btn btn-success d-none d-md-inline-block text-white">Add Data Baru <i class="fa-solid fa-plus"></i></a>
-                        </div>
-                    </div>
-                    <div class="my-4">
+                    <?php if(isset($_GET['add'])): ?>
+                        <form class="mt-2" action="" method="post">
+                            <div class="form-group">
+                                <label for="id_petugas">ID Petugas</label>
+                                <input id="id_petugas" name="id_petugas" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="nama">Nama</label>
+                                <input id="nama" name="nama" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="no_hp">No Hp</label>
+                                <input id="no_hp" name="no_hp" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="alamat">Alamat</label>
+                                <input id="alamat" name="alamat" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input id="username" name="username" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="id_role">Nama Role</label>
+                                <input id="id_role" name="id_role" type="text" class="form-control">
+                            </div>
+
+                            <button type="submit" class="btn btn-block btn-success" name="create">Tambah</button>
+                        </form>
+                    <?php endif?>
+
+                    <?php if(isset($_GET['edit'])): ?>
+                        <form class="mt-2" action="" method="post">
+                            <div class="form-group">
+                                <label for="id_petugas">Id Petugas</label>
+                                <input id="id_petugas" name="id_petugas" type="text" class="form-control" value="<?= $_GET['edit']?>" disabled>
+                            </div>
+                            <div class="form-group">
+                                <label for="nama">Nama</label>
+                                <input id="nama" name="nama" type="text" class="form-control" value="<?= $_GET['nama']?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="no_hp">No Hp</label>
+                                <input id="no_hp" name="no_hp" type="text" class="form-control" value="<?= $_GET['no_hp']?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="alamat">Alamat</label>
+                                <input id="alamat" name="alamat" type="text" class="form-control" value="<?= $_GET['alamat']?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input id="username" name="no_hp" type="text" class="form-control" value="<?= $_GET['username']?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="id_role">Nama Role</label>
+                                <input id="id_role" name="id_role" type="text" class="form-control" value="<?= $_GET['id_role']?>">
+                            </div>
+                            <button type="submit" class="btn btn-block btn-success" name="update" value="<?= $_GET['edit']?>">Ubah</button>
+                        </form>
+                    <?php endif?>
+
+                    <?php if(!isset($_GET['add']) && !isset($_GET['edit'])): ?>
+                        <div class="my-4">
                         <table id="example" class="table table-striped" style="width:100%">
                             <thead>
                                 <tr>
                                     <th>No</th>
-                                    <th>Nama </th>
+                                    <th>Nama</th>
                                     <th>No Hp</th>
                                     <th>Alamat</th>
                                     <th>Username</th>
                                     <th>Role</th>
                                     <th>Aksi</th>
+                                    <th></th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php 
-                                    $result = mysqli_query($conn, "SELECT pt.id_petugas ,pt.nama,pt.no_hp,pt.alamat,pt.username, rl.nama_roles FROM petugas pt JOIN roles rl ON pt.id_role = rl.id_role;");
-                                    $number = 1;
-                                    while($data = mysqli_fetch_array($result)) {         
-                                        echo "<tr>";
-                                        // echo "<td>".$data['id_gudang']."</td>";
-                                        echo "<td>".$number++."</td>";
-                                        echo "<td>".$data['nama']."</td>";
-                                        echo "<td>".$data['no_hp']."</td>";    
-                                        echo "<td>".$data['alamat']."</td>";
-                                        echo "<td>".$data['username']."</td>";
-                                        echo "<td>".$data['nama_roles']."</td>";        
-                                        echo "<td><a href='edit.php?id=$data[id_petugas]' class='btn btn-warning'><i class='fa-solid fa-pen-to-square'></i> Edit</a>
-                                        <a href='edit.php?id=$data[id_petugas]' class='btn btn-danger'><i class='fa-solid fa-xmark'></i> Delete</a></td></tr>";        
-                                    }
-                                ?>
+                                <?php $result = mysqli_query($conn, "SELECT * FROM petugas"); ?>
+                                <?php while ($data = mysqli_fetch_array($result)): ?>
+                                    <tr>
+                                        <td><?= $data['id_petugas'] ?></td>
+                                        <td><?= $data['nama'] ?></td>
+                                        <td><?= $data['no_hp'] ?></td>
+                                        <td><?= $data['alamat'] ?></td>
+                                        <td><?= $data['username'] ?></td>
+                                        <td><?= $data['id_role'] ?></td>
+                                        <td class="d-flex gap-3">
+                                            <a class="btn btn-outline-primary" href="?edit=<?= $data['id_petugas'] ?>&nama=<?= $data['nama']?>&no_hp=<?= $data['no_hp']?>&alamat=<?= $data['alamat']?>&username=<?= $data['username']?>&id_role=<?= $data['id_role']?>">Ubah</a>
+
+                                            <form action="" method="post">
+                                                <button type="submit" class="btn btn-outline-danger" name="delete" value="<?= $data['id_petugas'] ?>">Hapus</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                <?php endwhile?>
                             </tbody>
                         </table>
                     </div>
+                    <?php endif?>
 
                     </div>
                 </div>        
